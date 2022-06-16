67ead1dc0db43748c3b90ef2f34ed08447cf4ec7
Abin John <abinjohnanto@gmail.com>
2022-06-16 18:25:48 +0530

    add event delegation to task complete and task delete functionalities

--- script.js
+++ script.js
@@ -27,14 +27,14 @@ function setDate() {
 function returnHTML(task) {
   return `<li class="task-entry">
         <button class="circle" type="button">
-          <svg xmlns="http://www.w3.org/2000/svg" class="circle-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+          <svg xmlns="http://www.w3.org/2000/svg" class="circle-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" data-button-type="taskComplete">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" data-button-type="taskComplete"/>
           </svg>
         </button>
         <p class="task">${task}</p>
         <button class="trash">
-          <svg xmlns="http://www.w3.org/2000/svg" class="trash-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+          <svg xmlns="http://www.w3.org/2000/svg" class="trash-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" data-button-type="taskDelete">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" data-button-type="taskDelete"/>
 </svg>
         </button>
       </li>`;
@@ -50,7 +50,6 @@ function taskInput(e) {
 
   const html = returnHTML(task);
   taskContainer.insertAdjacentHTML('beforeend', html);
-  addListener();
 
   if (checkIconEls.length >= 5) {
     taskInputField.setAttribute(
@@ -66,7 +65,7 @@ function taskInput(e) {
 }
 
 // ###########################
-//  MARK A TASK AS COMPLETE
+//   MARK A TASK AS COMPLETE
 // ###########################
 function markTaskComplete() {
   this?.parentElement.classList.toggle('complete');
@@ -107,17 +106,33 @@ document.addEventListener('keydown', function (keyEvent) {
 });
 
 // ######################
-//  EVENT LISTENERS
+//   ADD TASK HANDLERS
+// ######################
+function attachListener(e) {
+  e.preventDefault();
+  let target = e.target;
+  const buttonType = target.getAttribute('data-button-type');
+
+  if (!buttonType) return;
+
+  while (!target.parentElement.classList.contains('task-entry'))
+    target = target.parentElement;
+
+  switch (buttonType) {
+    case 'taskComplete':
+      markTaskComplete.call(target);
+      break;
+    case 'taskDelete':
+      deleteTask.call(target);
+      break;
+  }
+}
+
+// ######################
+//   EVENT LISTENERS
 // ######################
 taskInputForm.addEventListener('submit', taskInput);
-
-function addListener() {
-  const recentCircle = [...checkIconEls].at(-1);
-  recentCircle.addEventListener('click', markTaskComplete);
-
-  const recentTrash = [...trashIconEls].at(-1);
-  recentTrash.addEventListener('click', deleteTask);
-}
+taskContainer.addEventListener('click', attachListener);
 
 // ######################
 //   INIT
